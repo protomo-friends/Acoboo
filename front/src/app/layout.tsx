616a69c38dc2047a7863718b95828951f0f44af2
shinamon <76248816+shinamon610@users.noreply.github.com>
2024-03-17 17:59:20 +0900

    feat(app): 上部にタブを追加

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +14,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body>
+        <nav className="flex bg-gray-800 p-4 text-white">
+          <Link href="/" className="border p-1 mx-2">
+            入出金
+          </Link>
+          <Link href="accounts" className="border p-1 mx-2">
+            口座
+          </Link>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
