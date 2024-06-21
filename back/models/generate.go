025223150a554fcd5f6339e4dbe4410df36aa7d8
sysnote8main <sysnote8main@gmail.com>
2024-06-21 16:47:03 +0900

    fix api schema for minimum run

--- back/models/generate.go
+++ back/models/generate.go
@@ -1,3 +1,4 @@
 package models
 
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config cfg.yaml ../../openapi.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -package models -generate models -o models.gen.go ../../openapi.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -package models -generate echo-server -o api.gen.go ../../openapi.yaml
